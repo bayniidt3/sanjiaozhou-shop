8d9494ecbe70afe487de7c60c65316a514dd5ee6
cc <331201489@qq.com>
2026-07-12 20:45:41 +0800

    chore

--- web-site/next.config.ts
+++ web-site/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+    trailingSlash: true, // 防止刷新404
 };
 
 export default nextConfig;
