c41ecaa2e50e9153538206dc42de1c3e7028132f
cc <331201489@qq.com>
2026-07-12 21:02:10 +0800

    chore

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -4,9 +4,10 @@ import path from "node:path";
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   basePath,
   outputFileTracingRoot: path.join(__dirname, ".."),
 };
 
 export default nextConfig;
+
+
